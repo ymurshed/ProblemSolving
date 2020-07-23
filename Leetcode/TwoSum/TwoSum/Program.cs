1b45d78f1e49e57eb72b3c41eef6cdea410b2aa8
Yaad Murshed <yaad.murshed@outlook.com>
2020-07-24 03:14:28 +0600

    Added two sum problem.

---
+++ Leetcode/TwoSum/TwoSum/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TwoSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var target = 6;
+            var nums = new[] {3, 2, 4};
+
+            var ob = new Solution();
+            var res = ob.TwoSum(nums, target);
+            Console.WriteLine(string.Join(",", res));
+        }
+    }
+}
---
+++ Leetcode/TwoSum/TwoSum/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+namespace TwoSum
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var indices = new int[2];
+            var numList = nums.ToList();
+            
+            for (var i = 0; i < numList.Count - 1; i++)
+            {
+                var secondNumber = target - numList[i];
+                var index = numList.FindIndex(i + 1, numList.Count - 1 - i, x => x == secondNumber);
+
+                if (index == -1) continue;
+
+                indices[0] = i;
+                indices[1] = index;
+                break;
+            }
+
+            return indices;
+        }
+    }
+}
