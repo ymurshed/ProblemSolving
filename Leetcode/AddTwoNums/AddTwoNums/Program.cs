4def2e2e2773f7442146c7788dac61edc96fdf2c
Yaad Murshed <yaad.murshed@outlook.com>
2020-07-26 16:38:43 +0600

    AddTwoNumber - medium.

---
+++ Leetcode/AddTwoNums/AddTwoNums/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AddTwoNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ob = new Solution();
+            var num1 = new List<int>
+            {
+                1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1
+            };
+            var num2 = new List<int>
+            {
+                5,6,4
+            };
+
+            var l1 = Solution.GetListNodeFromNumber(num1);
+            var l2 = Solution.GetListNodeFromNumber(num2);
+            var node = ob.AddTwoNumbers(l1, l2);
+            
+            while (node != null)
+            {
+                Console.Write($"{node.val}->");
+                node = node.next;
+            }
+        }
+    }
+}
---
+++ Leetcode/AddTwoNums/AddTwoNums/Solution.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AddTwoNums
+{
+
+    public class Solution
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            var num1 = GetNumbersFromListNode(l1);
+            var num2 = GetNumbersFromListNode(l2);
+            var num3 = Sum(num1, num2);
+            var node = GetListNodeFromNumber(num3);
+            return node;
+        }
+
+        public static List<int> GetNumbersFromListNode(ListNode listNode)
+        {
+            var numbers = new List<int>();
+            while (listNode != null)
+            {
+                numbers.Add(listNode.val);
+                listNode = listNode.next;
+            }
+            return numbers;
+        }
+
+        public static ListNode GetListNodeFromNumber(List<int> numbers)
+        {
+            var listNodes = new List<ListNode>();
+
+            foreach (var n in numbers)
+            {
+                var node = new ListNode(n);
+                listNodes.Add(node);
+            }
+
+            for (var i = 0; i < listNodes.Count - 1; i++)
+            {
+                listNodes[i].next = listNodes[i + 1];
+            }
+
+            return listNodes.FirstOrDefault();
+        }
+
+        private static List<int> Sum(IList<int> a, IList<int> b)
+        {
+            if (a.Count > b.Count)
+            {
+                var appendCount = a.Count - b.Count;
+                for (var i = 0; i < appendCount; i++)
+                {
+                    b.Add(0);
+                }
+            }
+            else if (b.Count > a.Count)
+            {
+                var appendCount = b.Count - a.Count;
+                for (var i = 0; i < appendCount; i++)
+                {
+                    a.Add(0);
+                }
+            }
+
+            var carry = 0;
+            var result = new List<int>();
+
+            for (var i = 0; i < a.Count; i++)
+            {
+                var sum = a[i] + b[i] + carry;
+                
+                if (sum > 9)
+                {
+                    sum = sum % 10;
+                    carry = 1;
+                }
+                else
+                {
+                    carry = 0;
+                }
+
+                result.Add(sum);    
+            }
+
+            if (carry == 1) result.Add(1);
+            return result;
+        }
+    }
+}
